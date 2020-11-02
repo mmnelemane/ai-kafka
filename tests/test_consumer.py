7b8430440801b2850e3d80ab2236d552a4ba7e52
Madhu Mohan Nelemane <mmnelemane@suse.com>
2020-11-02 12:42:06 +0100

    Added testfiles with just test names

---
+++ tests/test_consumer.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+
+# Tests to verify functionality of the consumer
+
+import consumer
+
+class AivenKafkaConsumerTest(object):
+    def __init__(self):
+        self.consumer = 
+        pass
+
+    def test_get_db_connection(self):
+        db_conn = self.consumer.get_db_connection(
+
+
+    def test_insert_db_record(self):
+        pass
+
+    def test_update_db_record(self):
+        pass
+
+    def test_run_consumer(self):
+        pass
+
+    def test_get_records(self):
+        pass
+
+
---
+++ tests/test_producer.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+# This test file is used for testing the producer functionality
+
+import producer
+
+class AivenKafkaProducerTest(object):
+    def __init__(self):
+        pass
+
+    def test_run_producer(self):
+        pass
+
+    def test_get_webmetrics(self) 
+        pass
+
